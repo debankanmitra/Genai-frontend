c68dc5b0159ca8ae92faf022e019370b1f616898
debankanmitra <debankanmitra@gmail.com>
2024-01-07 17:13:01 +0530

    chat interface done

--- src/chatpage/interface.jsx
+++ src/chatpage/interface.jsx
@@ -6,11 +6,44 @@ const Wrapper = styled.section`
     width: 90%;
     height: 75vh;
     /* border: 1px solid red; */
-    padding: 20px 25px 0px 25px;
+    overflow-y: scroll;
+    padding: 1.25rem 5rem 0rem 5rem;
     box-sizing: border-box;
     margin: 0% auto;
     border-radius: 25px 25px 0px 0px;
     background-color: rgba(30, 31, 32, 1);
+    &::-webkit-scrollbar {
+      display: none;
+    }
+    center {
+      margin: 0 0 3rem 0;
+      text-decoration: underline;
+    }
+    li {
+      list-style-type: none;
+      display: flex;
+    }
+    .user {
+      margin-bottom: 1rem;
+    }
+    .bot {
+      margin-bottom: 3rem;
+    }  
+    .user>span {
+        border: 1px solid rgba(87, 90, 123, 1);
+        padding: 2px 10px 4px 10px;
+        border-radius: 15px;
+        margin-right: 1.5rem;
+        background-color: rgba(87, 90, 123, 1);
+    }
+    .bot>span {
+        border: 1px solid rgba(102, 88, 234, 1);
+        padding: 2px 10px 4px 10px;
+        border-radius: 15px;
+        margin-right: 1rem;
+        background-color: rgba(102, 88, 234, 1);
+        height: fit-content;
+      }
   }
   .inputarea {
     /* border: 1px solid aliceblue; */
@@ -44,26 +77,46 @@ const Wrapper = styled.section`
 `;
 
 function Interface() {
-  const [inputValue, setInputValue] = useState('');
+  const [inputValue, setInputValue] = useState("");
   const [messages, setMessages] = useState([]);
 
   const handleSubmit = async (e) => {
     e.preventDefault();
     setMessages((prevMessages) => [...prevMessages, inputValue]);
-    const response= await fetch('http://127.0.0.1:8000/events', {
-      method: 'POST',
+    const response = await fetch("http://127.0.0.1:8000/events", {
+      method: "POST",
       body: JSON.stringify(inputValue),
       headers: {
-        'Content-Type': 'application/json'
-      }
+        "Content-Type": "application/json",
+      },
     });
+    setInputValue("");
     const result = await response.json();
     setMessages((prevMessages) => [...prevMessages, result.answer]);
-
+    console.log(inputValue);
   };
   return (
     <Wrapper>
-      <div>{messages?.map((message,index) => <p key={index}>{message}</p>)}</div>
+      <div className="screen">
+        <center>
+          {" "}
+          Hello! Ask me a question to get started. I am your personal
+          development coach.{" "}
+        </center>
+        {messages?.map((message, index) =>
+          index % 2 == 0 ? (
+            <li key={index} className="user">
+            <span>user</span>
+            <p>  {message}  </p>
+            </li>
+          ) : (
+            <li key={index} className="bot">
+            <span>coach</span>
+            <p>  {message}  </p>
+            </li>
+          )
+        )}
+      </div>
       <div className="inputarea">
         <form className="innerarea" onSubmit={handleSubmit}>
           <input
@@ -72,7 +125,7 @@ function Interface() {
             value={inputValue}
             placeholder="Talk to your Personal Development Coach"
             onChange={(e) => setInputValue(e.target.value)}
-            onKeyDown={(event) => event.key === "Enter" && handleSubmit()}
+            onKeyDown={(event) => event.key === "Enter" && handleSubmit(event)}
           />
           <button type="submit" className="common">
             Send
